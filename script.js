12fb745e948f40011a1a5769a2ee9b198ab3a30d
KunalKothari1810 <83830335+KunalKothari1810@users.noreply.github.com>
2021-06-04 22:41:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
 }
-        
+    function simple_interest()
+    {
+    var p,t,r,si;
+    p = document.getElementById ("first").value;
+
+   // r = document.getElementById ().value;
+    r=slider.value;
+    t = document.getElementById ("third").value;
+    
+    si = parseInt((p*t*r)/100);
+    
+    y=parseInt(t)+2021;
+    
+    if(p<=0){
+        alert("Please enter a postive number");
+      
+    }
+    else{
+    document.getElementById ('num').innerHTML ="If you deposit <mark>" +p+ "</mark> at an interest rate of <mark>"+r+"% </mark>You will receive an amount of <mark>"+si+ "</mark> in the year <mark>"+y+" </mark>";
+    }
+}
+
+    
+    
